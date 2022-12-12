193e3952b9409d1c1b671a45b7c141844b919c4d
Jason Luo <jasonluo1902@gmail.com>
2022-12-12 17:07:58 +1100

    Initial commit

---
+++ controllers/posts.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import PostMessage from "../models/postMessage.js"; // access to real model
+
+export const getPosts = async (req, res) => {
+  //   res.send("THIS WORKS!");
+  try {
+    // if successful
+    const postMessages = await PostMessage.find(); // asynchronous
+
+    // console.log(postMessages);
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    // if unsuccessful
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  //   res.send("Post Creation");
+  const post = req.body;
+
+  const newPost = new PostMessage(post);
+
+  try {
+    await newPost.save();
+
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No post with selected id");
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    _id,
+    { ...post, _id },
+    {
+      new: true,
+    }
+  );
+
+  res.json(updatedPost);
+};
---
+++ models/postMessage.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema({
+  title: String,
+  message: String,
+  creator: String,
+  tags: [String], // array of Strings
+  selectedFile: String,
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+}); // each post is going to have the above properties
+
+// model
+const PostMessage = mongoose.model("PostMessage", postSchema);
+
+export default PostMessage;
+// exporting a moogoose model from postMessage file. from model, we can find, create, delete, update
